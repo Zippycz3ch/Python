ff248edc55a677b79a61d29b56ae1159bdf0f268
Zdenekslezak <39026332+Zdenekslezak@users.noreply.github.com>
2020-10-12 15:34:31 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,6 +1,19 @@
-sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
-result = '' #empty variable
-for s in sounds: #goes thru list sounds
-    result = result + s #add item from list to result
-    result = result.upper() #chamge the chars om result to upper case
-print(result)
+#sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+#result = '' #empty variable
+#for s in sounds: #goes thru list sounds
+#    result = result + s #add item from list to result
+#    result = result.upper() #chamge the chars om result to upper case
+#print(result)
+
+#nested list
+#nested_list = [[1, 2, 3],[4, 5, 6,],[7,8,9]]
+
+#for l in nested_list:    #prints every value in nested_list
+#    for char in l:
+#        print(char)
+        
+#print(nested_list[0][1])
+
+#coor = [[10.1155, 11.6668],[58.5588, 65.3214],[71.6666,63.7777]] 
+#for val in coor:    #prints every list in nested list
+#    print(val)
